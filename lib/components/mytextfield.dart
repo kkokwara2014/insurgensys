a393f47d6c79079c97df6648a1af5f43be1a0ac0
kkokwara <kkokwara2014@gmail.com>
2023-12-13 07:57:51 +0100

    Creating custom textformfield

---
+++ lib/components/mytextfield.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final bool obscureText;
+  final String hintText;
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.obscureText,
+    required this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+          hintText: hintText,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+    );
+  }
+}
