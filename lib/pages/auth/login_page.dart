6c24914ba5c087520445e096d087b067db80b5d5
kkokwara <kkokwara2014@gmail.com>
2023-12-13 08:19:26 +0100

    Creating login and sign up pages

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,1 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:insurgensys/components/mybutton.dart';
+import 'package:insurgensys/components/mytextfield.dart';
+import 'package:insurgensys/pages/auth/register_page.dart';
 
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Image.asset("lib/images/shield.png"),
+              const SizedBox(
+                height: 15,
+              ),
+              MyTextField(
+                controller: emailController,
+                obscureText: false,
+                hintText: "Email",
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextField(
+                controller: passwordController,
+                obscureText: true,
+                hintText: "Password",
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              MyButton(text: "Login", onTap: () {}),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text("Don't have account? "),
+                  TextButton(
+                      onPressed: () {
+                        Get.to(() => const RegisterPage());
+                      },
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/auth/register_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:insurgensys/components/mybutton.dart';
+import 'package:insurgensys/components/mytextfield.dart';
+import 'package:insurgensys/pages/auth/login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<RegisterPage> {
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final passwordRepeatController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Image.asset("lib/images/shield.png"),
+              const SizedBox(
+                height: 15,
+              ),
+              MyTextField(
+                controller: nameController,
+                obscureText: false,
+                hintText: "Name",
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextField(
+                controller: emailController,
+                obscureText: false,
+                hintText: "Email",
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextField(
+                controller: phoneController,
+                obscureText: false,
+                hintText: "Phone",
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextField(
+                controller: passwordController,
+                obscureText: true,
+                hintText: "Password",
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              MyTextField(
+                controller: passwordRepeatController,
+                obscureText: true,
+                hintText: "Repeat Password",
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              MyButton(text: "Sign Up", onTap: () {}),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text("Already have account? "),
+                  TextButton(
+                      onPressed: () {
+                        Get.to(() => const LoginPage());
+                      },
+                      child: const Text(
+                        "Sign In",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
